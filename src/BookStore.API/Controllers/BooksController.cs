1a5cd9214c6cc294c7f619ab7b93050629b115ec
Altayeb Ibrahim <ibrahim@MOALAJAH.AE>
2021-08-17 12:20:33 +0400

    Added swagger and API

---
+++ src/BookStore.API/Controllers/BooksController.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using BookStore.API.Dtos.Book;
+using BookStore.Domain.Interfaces;
+using BookStore.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Controllers
+
+{
+    [Route("api/[controller]")]
+    public class BooksController : MainController
+    {
+        private readonly IMapper _mapper;
+        private readonly IBookService _bookService;
+
+        public BooksController(IMapper mapper, IBookService bookService)
+        {
+            _mapper = mapper;
+            _bookService = bookService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var books = await _bookService.GetAll();
+
+            return Ok(_mapper.Map<IEnumerable<BookResultDto>>(books));
+
+        }
+
+         [HttpGet("{id:int}")]
+        
+        public async Task<IActionResult> GetById(int id)
+        {
+            var book = await _bookService.GetById(id);
+            if (book == null) return NotFound();
+
+            return Ok(_mapper.Map<BookResultDto>(book));
+        }
+
+        [HttpGet]
+        [Route("get-books-by-category/{categoryId:int}")]
+        public async Task<IActionResult> GetBooksByCategory(int categoryId)
+        {
+            var books = await _bookService.GetBooksByCategory(categoryId);
+            if (!books.Any()) return NotFound();
+            return Ok(_mapper.Map<IEnumerable<BookResultDto>>(books));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(BookAddDto bookDto)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var book = _mapper.Map<Book>(bookDto);
+
+            var bookResult =await  _bookService.Add(book);
+
+            if (bookResult == null) return BadRequest();
+            return Ok(_mapper.Map<BookResultDto>(bookResult));
+
+        }
+
+        [HttpPut("{id:int}")]
+       
+
+        public async Task<IActionResult> Update(int id,BookEditDto bookDto)
+        {
+            if (id != bookDto.Id) return BadRequest();
+            if (!ModelState.IsValid) return BadRequest();
+
+            await _bookService.Update(_mapper.Map<Book>(bookDto));
+            return Ok(bookDto);
+
+        }
+
+        [HttpDelete("{id:int}")]
+        
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var book = await _bookService.GetById(id);
+            if (book == null) return BadRequest();
+
+            await _bookService.Remove(book);
+            return Ok();
+
+        }
+        [Route("search-Book-with-category/{searchedValue}")]
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> SearchBookWithCategory(string searchedValue)
+        {
+            var books = _mapper.Map<List<Book>>(await _bookService.SearchBookWithCategory(searchedValue));
+            if (!books.Any()) return NotFound("No book was found");
+            return Ok(_mapper.Map<IEnumerable<BookResultDto>>(books)); 
+        }
+    }
+}
--- src/BookStore.API/Startup.cs
+++ src/BookStore.API/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BookStore.API.Configuration;
+using Microsoft.OpenApi.Models;
 
 namespace BookStore.API
 {
@@ -25,7 +27,18 @@ namespace BookStore.API
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
+            services.AddSwaggerGen(c=> 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title= "BookStore API",
+                    Version = "v1"
+                }
+                    );
+            });
+            services.ResolveDependencies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +48,12 @@ namespace BookStore.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c=>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore API v1");
+            }
+            );
 
             app.UseHttpsRedirection();
 
